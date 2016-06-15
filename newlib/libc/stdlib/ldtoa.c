ee64c37e85382feb6ebae96a64c35224897d0192
dfffffff <dflamand@gmail.com>
2016-06-15 09:34:55 -0400

    ok now, compiler fixed

--- newlib/libc/stdlib/ldtoa.c
+++ newlib/libc/stdlib/ldtoa.c
@@ -1002,9 +1002,6 @@ is permitted to have its high guard word nonzero.  */
 
 static int edivm(short unsigned int *den, short unsigned int *num, LDPARMS *ldp)
 {
-#ifdef __m6809__
-	abort ();
-#else
 int i;
 register unsigned short *p;
 unsigned long tnum;
@@ -1088,7 +1085,6 @@ for( i=0; i<NI; i++ )
 	num[i] = equot[i];
 
 return( (int )j );
-#endif /* __m6809__ */
 }
 
 
